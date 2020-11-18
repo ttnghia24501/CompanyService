b9b6831c452bc6a915324ad78fefb7220d093fba
ttnghia24501 <53210685+ttnghia24501@users.noreply.github.com>
2020-11-18 10:18:57 +0700

    Add project files.

---
+++ CompanyService/ICompanyservice.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CompanyService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICompanyservice" in both code and config file together.
+    [ServiceContract]
+    public interface ICompanyService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare,
+        UriTemplate = "api/v1/getEmployeeList"
+        )]
+        List<Employee> getEmployees();
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare,
+        UriTemplate = "api/v1/createEmployee"
+        )]
+        bool createEmployee(Employee newEmployee);
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare,
+        UriTemplate = "api/v1/editEmployee/{id}"
+        )]
+        bool editEmployee(string id, Employee newEmployee);
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare,
+        UriTemplate = "api/v1/deleteEmployee/{id}"
+
+        )]
+        bool deleteEmployee(string id);
+
+
+
+        ///depart ment
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare,
+        UriTemplate = "api/v1/getDepartmentList"
+        )]
+        List<Department> getDepartments();
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare,
+        UriTemplate = "api/v1/createDepartment"
+        )]
+        bool createDepartment(Department newDepartment);
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare,
+        UriTemplate = "api/v1/editDepartment/{id}"
+        )]
+        bool editDepartment(string id, Department newDepartment);
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Bare,
+        UriTemplate = "api/v1/deleteDepartment/{id}"
+
+        )]
+        bool deleteDepartment(string id);
+    }
+}
