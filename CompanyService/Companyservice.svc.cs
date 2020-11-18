5f40a6e797510155549665bb68e8213f685a6eda
ttnghia24501 <53210685+ttnghia24501@users.noreply.github.com>
2020-11-18 10:27:46 +0700

    cm1

--- CompanyService/Companyservice.svc.cs
+++ CompanyService/Companyservice.svc.cs
@@ -11,5 +11,44 @@ namespace CompanyService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Companyservice.svc or Companyservice.svc.cs at the Solution Explorer and start debugging.
     public class Companyservice : ICompanyService
     {
+        public bool createDepartment(Department newDepartment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool createEmployee(Employee newEmployee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool deleteDepartment(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool deleteEmployee(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool editDepartment(string id, Department newDepartment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool editEmployee(string id, Employee newEmployee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Department> getDepartments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> getEmployees()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
