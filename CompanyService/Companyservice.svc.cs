bcb13248819a98abdc6c38a38660c604de870f15
ttnghia24501 <53210685+ttnghia24501@users.noreply.github.com>
2020-11-18 10:32:25 +0700

    cm2

--- CompanyService/Companyservice.svc.cs
+++ CompanyService/Companyservice.svc.cs
@@ -9,46 +9,133 @@ namespace CompanyService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Companyservice" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Companyservice.svc or Companyservice.svc.cs at the Solution Explorer and start debugging.
-    public class Companyservice : ICompanyService
+    public class CompanyService : ICompanyService
     {
+        CompanyDataContext data = new CompanyDataContext();
         public bool createDepartment(Department newDepartment)
         {
-            throw new NotImplementedException();
+            try
+            {
+                data.Departments.InsertOnSubmit(newDepartment);
+                data.SubmitChanges();
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public bool createEmployee(Employee newEmployee)
         {
-            throw new NotImplementedException();
+            try
+            {
+                data.Employees.InsertOnSubmit(newEmployee);
+                data.SubmitChanges();
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public bool deleteDepartment(string id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var department = data.Departments.Where(c => c.DepartmentID == int.Parse(id)).FirstOrDefault();
+                data.Departments.DeleteOnSubmit(department);
+                data.SubmitChanges();
+
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public bool deleteEmployee(string id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var employee = data.Employees.Where(c => c.EmployeeID == int.Parse(id)).FirstOrDefault();
+                data.Employees.DeleteOnSubmit(employee);
+                data.SubmitChanges();
+
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public bool editDepartment(string id, Department newDepartment)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var department = data.Departments.Where(c => c.DepartmentID == int.Parse(id)).FirstOrDefault();
+                data.Departments.DeleteOnSubmit(department);
+                data.Departments.InsertOnSubmit(newDepartment);
+                data.SubmitChanges();
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public bool editEmployee(string id, Employee newEmployee)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var employee = data.Employees.Where(c => c.EmployeeID == int.Parse(id)).FirstOrDefault();
+                data.Employees.DeleteOnSubmit(employee);
+                data.Employees.InsertOnSubmit(newEmployee);
+                data.SubmitChanges();
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public List<Department> getDepartments()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var department = (from Deparment in data.Departments select Deparment).ToList();
+
+                return department;
+            }
+            catch
+            {
+                return null;
+            };
         }
 
         public List<Employee> getEmployees()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var employee = (from Employee in data.Employees select Employee).ToList();
+
+                return employee;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
